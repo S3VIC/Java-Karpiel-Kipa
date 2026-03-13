4a8ccc943c16678ad403c282da94f4067f84c05b
S3VIC <sewikk@zohomail.eu>
2026-03-13 22:17:03 +0100

    Karki project initialization

---
+++ src/main/java/karki/com/karki/KarkiApplication.java
@@ -0,0 +1,13 @@
+package karki.com.karki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/karki/com/karki/KarkiApplicationTests.java
@@ -0,0 +1,13 @@
+package karki.com.karki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
