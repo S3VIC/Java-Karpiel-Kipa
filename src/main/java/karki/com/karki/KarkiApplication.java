8b6faa59acc411796163fd33c2d8db10d07cbc3f
Bartosz Karpiel <pzpsofficial@gmail.com>
2026-03-13 23:40:49 +0100

    feat: add empty Project repository and wire it up in main app class

--- src/main/java/karki/com/karki/KarkiApplication.java
+++ src/main/java/karki/com/karki/KarkiApplication.java
@@ -2,8 +2,12 @@ package karki.com.karki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "karki.com.karki.entity")
+@EnableJpaRepositories(basePackages = "karki.com.karki.repository")
 public class KarkiApplication {
 
     public static void main(String[] args) {
