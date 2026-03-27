7154aaed2539a06a123af8a38e9aa8302e31b7b8
Bartosz Karpiel <pzpsofficial@gmail.com>
2026-03-27 15:57:45 +0100

    feat: add user and task repositories

---
+++ src/main/java/karki/com/karki/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package karki.com.karki.repository;
+
+import karki.com.karki.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
